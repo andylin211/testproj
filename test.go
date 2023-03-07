fe9fd501fc18497d3ca7205a3e49d1521e11e05b
andylin211 <2273941148@qq.com>
2023-03-07 15:02:00 +0800

    test

--- test.go
+++ test.go
@@ -1,7 +1,19 @@
 package main
 import "github.com/xxx/testlib"
+
+
+type Person struct {}
+func (p *Person) foo(x string) {
+    e := testlib.NewEngine()
+    e.Where(x)
+}
+
 func main() {
     inp := __input()
-	e := testlib.NewEngine()
+    e := testlib.NewEngine()
     e.Where(inp)
+
+    inp := __input()
+    e := testlib.Person()
+    e.foo(inp)
 }
